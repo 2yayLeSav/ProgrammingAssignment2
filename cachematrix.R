2dd9c44a6169972ae0436decc66d42d8cfbd9139
JonDuVeme <gonefishinapostrophe@gmail.com>
2015-01-25 14:08:53 -0800

    Draft Functions Added
    Draft Functions Added - NOT YET WORKING

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
-## Put comments here that give an overall description of what your
-## functions do
+## A series of functions that caches the Inverse of a Matrix
 
-## Write a short comment describing this function
+## This first function creates a special "matrix" object that can cache its inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        reverse <- NULL
+        set <- function(y) {
+                x <<- y
+                reverse <<- NULL
+        }
+        get <- function() x
+        setrev <- function(solve) reverse <<- solve
+        getrev <- function() solve
+        list(set = set, get = get,
+             setrev = setrev,
+             getrev = getrev)
 }
 
-
-## Write a short comment describing this function
+## This function computes the inverse of the special "matrix" returned by 
+##makeCacheMatrix above. If the inverse has already been calculated 
+##(and the matrix has not changed), then the cachesolve should retrieve the 
+##inverse from the cache.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        rev <- x$getrev()
+        if(!is.null(rev)) {
+                message("getting cached data")
+                return(rev)
+        }
+        data <- x$get()
+        rev <- solve(data, ...)
+        x$setrev(rev)
+        rev
 }
